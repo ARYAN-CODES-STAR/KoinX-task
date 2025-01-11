45209d5d5947eec58757b45da3128a5f4096fe35
Aryan Sharma <aryansh0004@gmail.com>
2025-01-11 17:53:25 +0530

    remove use hook

--- my-project/app/[coinId]/[section]/page.tsx
+++ my-project/app/[coinId]/[section]/page.tsx
@@ -1,13 +1,15 @@
-import { use } from 'react';
 import CoinDetails from '../../components/CoinDetails';
 
-export default function CoinDetailsPage({ 
-  params 
-}: { 
-  params: { coinId: string; section: string } 
-}) {
-  const coinId = use(Promise.resolve(params.coinId)) || 'bitcoin';
-  const section = use(Promise.resolve(params.section)) || 'overview';
+type PageProps = {
+  params: {
+    coinId: string;
+    section: string;
+  }
+}
+
+export default function CoinDetailsPage({ params }: PageProps) {
+  const coinId = params.coinId || 'bitcoin';
+  const section = params.section || 'overview';
 
   return <CoinDetails initialCoinId={coinId} initialSection={section} />;
 }
